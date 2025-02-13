d599960f74deeb4353d29f9c8847bb50607c0286
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-13 20:20:53 +0530

    search result component

---
+++ src/components/SearchResultList.jsx
@@ -0,0 +1,13 @@
+import "./SearchResultList.css"
+
+const SearchResultList = ({results}) => {
+  return (
+    <div className="results-list">
+      {results.map((result, id) => {
+        return <div key={id}>{result.name}</div>
+      })}
+    </div>
+  )
+}
+
+export default SearchResultList
