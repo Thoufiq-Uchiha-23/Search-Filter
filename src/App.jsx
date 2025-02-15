8abe4890a0629dc9d1898ec75f881cba49218ebf
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-15 23:43:33 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // import { useState } from 'react'
 import { useState } from "react";
 import "./App.css";
-import SearchBar from "./components/SearchBar";
-import SearchResultList from "./components/SearchResultList";
+// import SearchBar from "./components/SearchBar";
+// import SearchResultList from "./components/SearchResultList";
+import SearchComponent from "./components/Search Component/SearchComponent";
 function App() {
   const [results, setResults] = useState([]);
 
   return (
     <div className="App">
       <div className="search-bar-container">
-        <SearchBar setResults={setResults} />
-        <SearchResultList results={results} />
+        {/* <SearchBar setResults={setResults} />
+        <SearchResultList results={results} /> */}
+        <SearchComponent setResults={setResults} results={results} />
       </div>
     </div>
   );
