efce16ab1e78c976cede47057dd30658ec99f92a
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-16 11:15:45 +0530

    Search Component is now fixed

--- src/components/Search Component/SearchComponent.jsx
+++ src/components/Search Component/SearchComponent.jsx
@@ -2,62 +2,64 @@ import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "../SearchBar.css"
 import "../SearchResult.css"
-import "../SearchResultList"
+import "../SearchResultList.css"
 
-const SearchComponent = ({ setResults, results }) => {
-  const SearchBar = ({setResults}) => {
-    const [input, setInput] = useState("");
+export const SearchBar = ({setResults}) => {
+  const [input, setInput] = useState("");
 
-    const fetchData = (value) => {
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((json) => {
-          const results = json.filter((user) => {
-            return (
-              value &&
-              user &&
-              user.name &&
-              user.name.toLowerCase().includes(value.toLowerCase()) // Fix here
-            );
-          });
-
-          setResults(results);
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value.toLowerCase()) // Fix here
+          );
         });
-    };
 
-    const handleChange = (value) => {
-      setInput(value);
-      fetchData(value);
-    };
-    return (
-      <div className="input-wrapper">
-        <FaSearch id="search-icon" />
-        <input
-          placeholder="Type to search ..."
-          value={input}
-          onChange={(e) => handleChange(e.target.value)}
-        />
-      </div>
-    );
+        setResults(results);
+      });
   };
 
-  const SearchResult = ({result}) => {
-    return (
-      <div className="search-result" onClick={(e) => alert(`You clicked on ${result.name}`)}>
-        {result.name}
-      </div>
-    )
-  }
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search ..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
 
-  const SearchResultList = ({results}) => {
-    return (
-      <div className="results-list">
-        {results.map((result, id) => {
-          return <SearchResult key={id} result={result}/>
-        })}
-      </div>
-    )
-  }
+export const SearchResult = ({result}) => {
+  return (
+    <div className="search-result" onClick={(e) => alert(`You clicked on ${result.name}`)}>
+      {result.name}
+    </div>
+  )
+}
+
+export const SearchResultList = ({results}) => {
+  return (
+    <div className="results-list">
+      {results.map((result, id) => {
+        return <SearchResult key={id} result={result}/>
+      })}
+    </div>
+  )
+}
+
+const SearchComponent = ({ setResults, results }) => {
+  
   return (
     <div>
       <SearchBar setResults={setResults} />
